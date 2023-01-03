4301b29c05ff1bbf43ae517e6845bb8380cb2ba6
Jesse Dorman <Jesse.dorman@us.af.mil>
2023-01-03 18:23:28 +0000

    remove imports/add verse link example

--- main.py
+++ main.py
@@ -2,28 +2,25 @@ from kivymd.app import MDApp
 from kivymd.uix.widget import MDWidget
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivymd.uix.dialog import MDDialog
 import webbrowser
-import requests
-from bs4 import BeautifulSoup as bs
-import re
-import pandas as pd
-import urllib.request as img_get
 
 Window.size = 640, 640
 
 class MainWindow(MDWidget):
 
     def jan1(self):
-        pass
-    
+        webbrowser.open_new_tab("https://www.blueletterbible.org/tools/MultiVerse.cfm?s=004b4A")
+            
     def jan8(self):
-        pass
+        webbrowser.open_new_tab("https://www.blueletterbible.org/tools/MultiVerse.cfm?s=008QpB")
     
     def jan15(self):
-        pass
+        webbrowser.open_new_tab("https://www.blueletterbible.org/tools/MultiVerse.cfm?s=008QpD")
 
     def donate(self):
-        webbrowser.open_new_tab("")
+        pass
+        # webbrowser.open_new_tab("")
 
     def close_two(self):
         exit()
