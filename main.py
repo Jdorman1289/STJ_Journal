2f221311c58ac00c8e19a9495d3f30682761c012
Jesse Dorman <Jesse.dorman@us.af.mil>
2023-01-05 15:54:22 +0000

    Start changes to add a sep dedicated notes screen

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from kivymd.app import MDApp
-from kivymd.uix.widget import MDWidget
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-from kivymd.uix.label import MDLabel
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.card import MDCard
 import webbrowser
 
 Window.size = 640, 640
@@ -12,7 +9,7 @@ Window.size = 640, 640
 
 saved_list = []
 
-class MainWindow(MDWidget):
+class MainWindow(Screen):
 
     def jan1(self):
         webbrowser.open_new_tab("https://www.blueletterbible.org/tools/MultiVerse.cfm?s=004b4A")
@@ -23,32 +20,15 @@ class MainWindow(MDWidget):
     def jan15(self):
         webbrowser.open_new_tab("https://www.blueletterbible.org/tools/MultiVerse.cfm?s=008QpD")
 
-    def save_entry(self):
-        # Get the input fields and card
-        title_input = self.ids['title_input']
-        body_input = self.ids['body_input']
-        entry_card = self.ids['entry_card']
 
-        # Get the values from the input fields
-        title = title_input.text
-        body = body_input.text
-        
-        new_card = MDCard()
-        box_layout = MDBoxLayout(orientation='vertical')
+class NotesWindow(Screen):
+    pass
 
-        entry_card.add_widget(new_card)
-        new_card.add_widget(box_layout)
+class NavBar(Screen):
+    pass
 
-        # Add the title and body as labels to the box layout
-        box_layout.add_widget(MDLabel(text=title, font_style='H6'))   
-        box_layout.add_widget(MDLabel(text=body))
-
-        # Set the elevation of the card
-        new_card.elevation = 2
-
-        # Clear the input fields
-        title_input.text = ''
-        body_input.text = ''
+class WindowManager(ScreenManager):
+    pass
 
 class STJ(MDApp):
     def build(self):
