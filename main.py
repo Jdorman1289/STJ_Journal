23c4bc61924a207262fe465f5ebee0cecdfc78d3
Jesse Dorman <jdorman1289@gmail.com>
2023-01-06 17:32:45 -0600

    start the logic of saving

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.list import OneLineIconListItem
+from kivy.properties import StringProperty
+from kivymd.uix.dialog import MDDialog
 from kivy.core.window import Window
 import webbrowser
 
 Window.size = 640, 640
 
+class SettingsList(OneLineIconListItem):
+    divider = None
+    source = StringProperty()
 
 class MainWindow(Screen):
 
@@ -24,6 +30,9 @@ class NotesWindow(Screen):
     def add_note(self):
         #to be added
         pass
+    def discard_note(self):
+        #to be added
+        pass
 
 
 class NavBar(Screen):
@@ -36,6 +45,22 @@ class STJ(MDApp):
 
     def set_screen(self, screen_name):
         self.root.current = screen_name
+
+    dialog = None
+
+    def notes_settings(self):
+
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Settings",
+                type="simple",
+                items=[
+                    SettingsList(text="Save Note",source="content-save-outline"),
+                    SettingsList(text="Discard Draft",source="delete-alert-outline"),
+                ],
+            )
+        self.dialog.open()
+
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
