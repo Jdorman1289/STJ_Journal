39664d4d1966585e92f394b85a9a3a476ba3d768
Jesse Dorman <Jesse.dorman@us.af.mil>
2023-01-19 20:11:23 +0000

    add saving notes to a json

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+import json
 import webbrowser
 
 Window.size = 480, 640
@@ -97,6 +98,11 @@ One thing you are thankful for tonight:
     def save_note(self):
         persistent_notes[self.day_selected] = self.ids.journal_questions_text.text
 
+        json_notes = json.dumps(persistent_notes)
+        f = open("notes.json","w")
+        f.write(json_notes)
+        f.close
+
     def jan1(self):
         webbrowser.open_new_tab("https://www.blueletterbible.org/tools/MultiVerse.cfm?s=004b4A")
             
@@ -111,26 +117,6 @@ class WindowManager(ScreenManager):
     pass
 
 class STJ(MDApp):
-
-
-    # def edit_saved_item(self, title, body):
-    #     screen_manager = self.root
-    #     notes_window = screen_manager.get_screen("notes_window")
-    #     notes_window.ids.note_text.text = body
-    #     self.dialog.content_cls.ids.note_title.text = title.text
-        
-
-    # def add_to_list(self, note_title):
-    #     screen_manager = self.root
-    #     notes_window = screen_manager.get_screen("notes_window")
-    #     list_of_notes = notes_window.ids.list_of_notes
-    #     note_text = notes_window.ids.note_text.text
-
-    #     persistent_notes[note_title.text] = note_text
-
-    #     for key, value in persistent_notes.items():
-   
-
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
